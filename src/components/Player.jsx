c508b02ccfbf81ef2429d90cdd1cf0abdd201d61
sobbey99 <sobbey99@gmail.com>
2023-06-25 23:25:34 +0300

    player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,9 +4,13 @@ import { Vector3 } from "three";
 import { useEffect, useRef } from "react";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
+
 const Player = () => {
-  const actions = useKeyboard();
-  console.log(Object.entries(actions).filter(([k, v]) => v));
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboard();
+  // console.log(Object.entries(actions).filter(([k, v]) => v));
 
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
@@ -36,7 +40,31 @@ const Player = () => {
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
 
-    // api.velocity.set(0, 1, 0);
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
   });
 
   return <mesh ref={ref}></mesh>;
